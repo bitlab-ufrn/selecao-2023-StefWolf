0df0b0ffcc7888b689ed5329ebcc77240561ca93
StefWolf <stefaneassisori@gmail.com>
2023-04-12 01:28:09 +0100

    implementando biblioteca Piii em componente DetectBadWord #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { detectBadWord } from './components/DetectBadWord'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let result = detectBadWord("caralho")
+  console.log(result)
 
   return (
     <div className="App">
