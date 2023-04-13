696ad25f930bbae799e769bae1331eb7b56e2f20
StefWolf <stefaneassisori@gmail.com>
2023-04-13 10:54:38 +0100

    refatorando o formulário #2

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,11 @@ import translate from 'translate';
 import Footer from './components/Footer';
 import { useTheme } from './hooks/useTheme';
 import { FaMoon, FaSun } from "react-icons/fa";
+import Form from './components/Form';
 
 function App() {
 
   const {theme, setTheme} = useTheme();
-
-  const [text, setText] = useState("");
-  const [hasBadWord, setHadBadWord] = useState();
-  const [language, setLanguage] = useState("pt");
-  const [placeholder, setPlaceholder] = useState("Digite o texto");
-
-  translate.engine = "google";
-
-  const handleLetterTranslatorPortuguese = async function tradutor(text){
-    return await translate(text, {to: "pt"});
-  };
-
-  async function verifyWords(e) {
-    e.preventDefault();
-  
-    if(language !== "pt") {
-      setText(await handleLetterTranslatorPortuguese(text));
-    }
-    setHadBadWord(await detectBadWord(text));
-  }
 
    
   return (
@@ -45,21 +26,7 @@ function App() {
         }
       </div>
       <h1>Search Bad Words...</h1>
-      <form onSubmit={verifyWords} className='form'>
-        <input className='form__input' placeholder={placeholder} id='text' type='text' name='text'
-          icon={<BiSearchAlt size={25} />} onChange={(e) => setText(e.target.value)} required/>
-          <div className='flex justify-between mt-5'>
-            <select className='form__select' id="language" name="language" onChange={(e) => setLanguage(e.target.value)}>
-                <option value="pt" selected>Português</option>
-                <option value="">Outro Idioma</option>
-            </select>
-            <button className='form__button' type='submit'>Verificar</button>
-          </div>
-      </form>
-
-      <div className='laptop:w-[44%] w-[90%] mt-12'>
-        <ShowAlert status={hasBadWord} />
-      </div>
+      <Form />
       <p className=' dark:-text--text-color -text--text-color-l laptop:w-[50%] w-[80%] mt-5 mb-4'>
         <b>Observação:</b> Algumas mensagens podem conter conteúdo impróprio ou inofensivo, sendo assim, estamos aqui
         para lhe ajudar a verificar esse problemas e garantir uma melhor segurança à política das
---
+++ src/components/Form.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { detectBadWord } from './DetectBadWord'
+import { BiSearchAlt } from "react-icons/bi";
+import ShowAlert from './ShowAlert';
+import translate from 'translate';
+
+
+export default function Form(){
+
+    const [text, setText] = useState("");
+    const [hasBadWord, setHadBadWord] = useState();
+    const [language, setLanguage] = useState("pt");
+    const [placeholder, setPlaceholder] = useState("Digite o texto");
+
+    translate.engine = "google";
+
+    const handleLetterTranslatorPortuguese = async function tradutor(text){
+        return await translate(text, {to: "pt"});
+    };
+
+    async function verifyWords(e) {
+        e.preventDefault();
+    
+        if(language !== "pt") {
+        setText(await handleLetterTranslatorPortuguese(text));
+        }
+        setHadBadWord(await detectBadWord(text));
+    }
+
+    return(
+
+        <div className='w-full flex flex-col items-center justify-center'>
+            <form onSubmit={verifyWords} className='form'>
+                <input className='form__input' placeholder={placeholder} id='text' type='text' name='text'
+                icon={<BiSearchAlt size={25} />} onChange={(e) => setText(e.target.value)} required/>
+                <div className='flex justify-between mt-5'>
+                    <select className='form__select' id="language" name="language" onChange={(e) => setLanguage(e.target.value)}>
+                        <option value="pt" selected>Português</option>
+                        <option value="">Outro Idioma</option>
+                    </select>
+                    <button className='form__button' type='submit'>Verificar</button>
+                </div>
+            </form>
+
+            <div className='laptop:w-[44%] w-[90%] mt-12'>
+                <ShowAlert status={hasBadWord} />
+            </div>
+        </div>
+    )
+}
