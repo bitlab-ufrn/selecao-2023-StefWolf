9f3a98ef32f6d4b847f167952ba732136d2e1198
StefWolf <stefaneassisori@gmail.com>
2023-04-13 10:17:20 +0100

    transition em botoes de mode #2

--- src/App.jsx
+++ src/App.jsx
@@ -36,10 +36,10 @@ function App() {
     <body className='body'>
       <div className='w-[90%] laptop:mb-0 mb-7 flex justify-end'>
         {theme === "ligth" ? (
-          <button className='bg-black/10 p-3 gap-2 text-blue-gray-900 rounded-lg flex' onClick={()=> setTheme("dark")}>
+          <button className='bg-black/10 transition-all hover:-bg--background-color hover:-text--text-color p-3 gap-2 text-blue-gray-900 rounded-lg flex' onClick={()=> setTheme("dark")}>
             <FaMoon size={20} /><p>Escuro</p>
           </button>) 
-        :(<button className='bg-black/70 p-3 gap-2 -text--text-color rounded-lg flex' onClick={()=> setTheme("ligth")}>
+        :(<button className='bg-black/70 p-3 gap-2 hover:-bg--background-color-l hover:-text--text-color-l transition-all -text--text-color rounded-lg flex' onClick={()=> setTheme("ligth")}>
           <FaSun size={20} /><p>Claro</p>
           </button>)
         }
