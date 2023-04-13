05dfb1aaa47fe40920d37be37a229b39bbb8125f
StefWolf <stefaneassisori@gmail.com>
2023-04-14 00:29:35 +0100

    últimos acréscimos ao footer #2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,11 @@ export default function Footer(){
     return(
         <footer>
             <p>&#169; 2023 - Segurança Online</p>
+            <div className="footer__links">
+                <a href="">Terms</a>
+                <a href="">Github</a>
+                <a href="">About</a>
+            </div>
         </footer>
     )
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -30,8 +30,13 @@ export default function Form(){
 
         <div className='w-full flex flex-col items-center justify-center'>
             <form onSubmit={verifyWords} className='form'>
-                <input className='form__input' placeholder="Digite o texto" id='text' type='text' name='text'
-                icon={<BiSearchAlt size={25} />} onChange={(e) => setText(e.target.value)} required/>
+                
+                <input className='form__input' 
+                    placeholder="Digite o texto" id='text' type='text' name='text'
+                    icon={<BiSearchAlt size={25} />} 
+                    onChange={(e) => setText(e.target.value)} 
+                    required/>
+
                 <div className='flex justify-between mt-5'>
                     <select className='form__select' id="language" name="language" onChange={(e) => setLanguage(e.target.value)}>
                         <option value="pt" selected>Português</option>
