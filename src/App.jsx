3069ab3949d7536cc4709f04f17ae5c945d63419
StefWolf <stefaneassisori@gmail.com>
2023-04-13 02:49:20 +0100

    adicionando footer #2

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,31 @@ import { detectBadWord } from './components/DetectBadWord'
 import { BiSearchAlt } from "react-icons/bi";
 import ShowAlert from './components/ShowAlert';
 import translate from 'translate';
+import Footer from './components/Footer';
 
 function App() {
 
-  const [text, setText] = useState("")
-  const [hasBadWord, setHadBadWord] = useState()
-  const [language, setLanguage] = useState("pt")
-  const [placeholder, setPlaceholder] = useState("Digite o texto")
+  const [text, setText] = useState("");
+  const [hasBadWord, setHadBadWord] = useState();
+  const [language, setLanguage] = useState("pt");
+  const [placeholder, setPlaceholder] = useState("Digite o texto");
 
-  const handleLetterTranslator =  async function tradutor(text, language){
-    translate.engine= "google";
-    return await translate(text, {to: language});
-  }
+  translate.engine = "google";
 
-  const handleLetterTranslatorPortuguese =  async function tradutor(text){
-    translate.engine= "google";
+  const handleLetterTranslatorPortuguese = async function tradutor(text){
     return await translate(text, {to: "pt"});
-  }
+  };
 
   async function verifyWords(e) {
-    e.preventDefault()
+    e.preventDefault();
   
-    if(language != "pt")
-     setText(await handleLetterTranslatorPortuguese(text))
-    
-    setHadBadWord(await detectBadWord(text))
+    if(language !== "pt") {
+      setText(await handleLetterTranslatorPortuguese(text));
+    }
+    setHadBadWord(await detectBadWord(text));
   }
 
-  useEffect(() => {
-    async function modifyPlaceholder(){
-      if(language != "pt"){
-        setPlaceholder(await handleLetterTranslator(placeholder, language))
-        console.log(placeholder)
-      }
-    }
-    modifyPlaceholder()
-  })
-
-  
+   
   return (
     <body className='body'>
       <h1>Search Bad Words...</h1>
@@ -50,9 +37,7 @@ function App() {
           <div className='flex justify-between mt-5'>
             <select className='form__select' id="language" name="language" onChange={(e) => setLanguage(e.target.value)}>
                 <option value="pt" selected>Português</option>
-                <option value="en">English</option>
-                <option value="es">Spanish</option>
-                <option value="fr">French</option>
+                <option value="">Outro Idioma</option>
             </select>
             <button className='form__button' type='submit'>Verificar</button>
           </div>
@@ -61,7 +46,7 @@ function App() {
       <div className='laptop:w-[44%] w-[90%] mt-12'>
         <ShowAlert status={hasBadWord} />
       </div>
-
+      <Footer />
     </body>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+
+export default function Footer(){
+    return(
+        <footer>
+            <p>&#169; 2023 - Segurança Online</p>
+        </footer>
+    )
+}
