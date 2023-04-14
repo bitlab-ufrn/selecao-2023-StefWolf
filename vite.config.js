0cdc3e2b7ff02b8e6901f9a7738d1fa26bfce989
StefWolf <stefaneassisori@gmail.com>
2023-04-14 02:59:33 +0100

    correção rollupOptions/external in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['piii'],
-      output: {
-        globals: {
-          piii: 'piii',
-        }
-      }
+      external: ['piii']
     }
   },
   plugins: [react()],
