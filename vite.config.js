d4345c82a979f1f6cc56007e6925b176f7350427
StefWolf <stefaneassisori@gmail.com>
2023-04-14 02:25:42 +0100

    correção titulo da página

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['piii'],
+      output: {
+        globals: {
+          piii: 'Piii',
+        }
+      }
+    }
+  },
   plugins: [react()],
 })
