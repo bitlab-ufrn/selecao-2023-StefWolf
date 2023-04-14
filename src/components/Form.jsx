066e31860a2f9bd69cbe7f27b3044aec62e088c7
StefWolf <stefaneassisori@gmail.com>
2023-04-14 13:40:57 +0100

    alterando placeholder e texto para o espanhol #5

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,21 +10,32 @@ export default function Form(){
     const [text, setText] = useState("")
     const [hasBadWord, setHadBadWord] = useState()
     const [language, setLanguage] = useState("pt")
+    const [placeholder, setPlaceholder] = useState("Digite seu texto aqui")
 
     translate.engine = "google"
 
-    const handleLetterTranslatorEnglish = async function tradutor(text){
-        return await translate(text, { from: "pt", to: "en" })
+    const handleLetterTranslator = async function tradutor(previous, current, text){
+        return await translate(text, { from: previous, to: current })
     };
 
     async function verifyWords(e) {
         e.preventDefault();
     
-        if(language === "pt"){
-            setHadBadWord(detectBadWord(await handleLetterTranslatorEnglish(text)))
+        if(language != "en"){
+            setHadBadWord(detectBadWord(await handleLetterTranslator(language, "en", text)))
         } else {
             setHadBadWord(detectBadWord(text))
         }
+    }
+
+    async function handleModifyLanguage(_language){
+        if(_language != "pt")
+            setPlaceholder(await handleLetterTranslator("pt", _language, placeholder))
+
+        if(_language === "pt")
+            setPlaceholder("Digite seu texto aqui")
+        
+        setLanguage(_language)
     }
 
     return(
@@ -33,15 +44,16 @@ export default function Form(){
             <form onSubmit={verifyWords} className='form'>
 
                 <input className='form__input' 
-                    placeholder="Digite o texto" id='text' type='text' name='text'
+                    placeholder={placeholder} id='text' type='text' name='text'
                     icon={<BiSearchAlt size={25} />} 
                     onChange={(e) => setText(e.target.value)} 
                     required/>
 
                 <div className='flex justify-between mt-5'>
-                    <select className='form__select' id="language" name="language" onChange={(e) => setLanguage(e.target.value)}>
+                    <select className='form__select' id="language" name="language" onChange={(e) => handleModifyLanguage(e.target.value)}>
                         <option value="pt" selected>Português</option>
                         <option value="en">English</option>
+                        <option value="es">Spanish</option>
                     </select>
                     <button className='form__button' type='submit'>Verificar</button>
                 </div>
